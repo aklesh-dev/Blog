30badf3cbe3543b7d3f9ceb6c94f0144d4386fe1
aklesh-dev <arzunayavadu998@gmail.com>
2024-09-05 17:34:13 +0545

    add functionality to the sign up page and add proxy server

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,49 @@
-import { Button, FloatingLabel } from 'flowbite-react';
-import React from 'react'
-import { Link } from 'react-router-dom';
+import { Alert, Button, FloatingLabel, Spinner } from 'flowbite-react';
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { HiInformationCircle } from "react-icons/hi";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill in all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+
+  };
+
   return (
     <section className="min-h-screen mt-20">
       <div className="flex gap-5 p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
@@ -18,19 +59,34 @@ export default function SignUp() {
 
         {/* right side */}
         <div className="flex-1">
-          <form className='flex flex-col gap-4'>
-            <FloatingLabel className='' variant='outlined' label='Username' id='username' type='text' />
-            <FloatingLabel variant='outlined' label='Email' id='email' type='email' />
-            <FloatingLabel variant='outlined' label='Password' id='password' type='password' />         
-            
-            <Button gradientDuoTone="purpleToPink" className='uppercase' type='submit'>Sign Up</Button>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <FloatingLabel onChange={handleChange} variant='outlined' label='Username' id='username' type='text' />
+            <FloatingLabel onChange={handleChange} variant='outlined' label='Email' id='email' type='email' />
+            <FloatingLabel onChange={handleChange} variant='outlined' label='Password' id='password' type='password' />
+
+            <Button gradientDuoTone="purpleToPink" className='uppercase' type='submit' disabled={loading}>
+              {loading ?
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </> : 'sign up'
+              }
+            </Button>
           </form>
 
           <div className="flex gap-2 mt-4 text-sm">
             <span>Already have an account ?</span>
             <Link to='/sign-in' className='text-blue-500 hover:text-blue-700'>Sign in</Link>
           </div>
+
+          {
+            errorMessage && <Alert className='mt-4' color='failure' icon={HiInformationCircle}>
+              <span className="font-semibold">Info alert! </span>{errorMessage}
+            </Alert>
+          }
+
         </div>
+
       </div>
     </section>
   )
