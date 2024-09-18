cdbb81b0863fb7dd53754d4ed60956359f811c2a
aklesh-dev <arzunayavadu998@gmail.com>
2024-09-18 09:51:17 +0545

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <section className="min-h-svh flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-justify">
+        <div>
+          <h1 className='text-3xl font-semibold text-center my-7'>About Aklesh's Blog</h1>
+          <div className="text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Aklesh's Blog! This blog is created by Aklesh Yadav as a personal project to share his knowledge and experiences in software development.
+              Aklesh is a passionate developer who loves to write about coding, programming, and technology.
+            </p>
+            <p>
+              On this blog, you will find tutorials, articles, and other resources that will help you learn and improve your programming skills.
+              You can also find tutorials on various programming languages, frameworks, and tools.
+            </p>
+            <p>
+              We encourage you leave comments on our posts and engage with other readers to help each other learn and grow.
+              You can like other user comments and reply to them as well. We believe that community of learners can help each other grow and improve their skills.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
-}
+};
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import CallToAction from '../components/CallToAction'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <section className="min-h-screen max-w-4xl mx-auto p-3 flex flex-col justify-center items-center gap-6">
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-gray-500'>Build fun and engaging projects while learning HTML, CSS, and JavaScript.</p>
+      
+      <CallToAction />
+    </section>
   )
 }
+
+
